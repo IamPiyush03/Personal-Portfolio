6fa3557c446b4fa7ee9d7b8a197ad4f10be3a8fd
IamPiyush03 <142411215+IamPiyush03@users.noreply.github.com>
2024-12-02 17:24:16 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,6 +1,7 @@
 import SortingVisualizerImg from "../assets/images/portfolio/SortingVisualizer.png";
 import FlavorFleetImg from "../assets/images/portfolio/FlavorFleet.png";
-import CryptoPortfolioImg from "../assets/images/portfolio/CryptoPortfolioImg.png"
+import CryptoPortfolioImg from "../assets/images/portfolio/CryptoPortfolioImg.png";
+import GasUtilityImg from "../assets/images/portfolio/GasUtility.png";
 
 const projectsData = [
   {
@@ -32,7 +33,18 @@ const projectsData = [
     source: "https://github.com/IamPiyush03/Crypto-Portfolio",
     description:
       "Crypto Portfolio is a web-based application that allows users to track their cryptocurrency investments in real time. Users can view live prices, market data, and detailed information about various cryptocurrencies, making it easier to manage their digital assets.",
-  }
+  },
+  ,
+  {
+    id: "gas-utility",
+    img: GasUtilityImg,
+    name: "Gas Utility Service",
+    stack: ["< Django />", "< PostgreSQL />", "< HTML5 />", "< CSS3 />"],
+    src: "https://github.com/IamPiyush03/Gas-Utility-Website",
+    source: "https://gas-utility-website-1.onrender.com", 
+    description:
+      "Gas Utility Service is a Django-based web application that allows users to request services, track their requests, and manage their account information. It features user authentication and a user-friendly interface for managing gas utility services.",
+  },
 ];
 
 export { projectsData };
